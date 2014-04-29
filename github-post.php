4d2309e0d16e0efabfa025cb7e4f82bd2a23f89f
Scott Block <scottrblock@gmail.com>
2014-04-29 19:51:23 -0400

    change pull thing

--- github-post.php
+++ github-post.php
@@ -1,10 +1,9 @@
 <?php
  
 // Use in the "Post-Receive URLs" section of your GitHub repo.
- 
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd ~/apps/wordpress/htdocs/ && git reset --hard HEAD && git pull' );
-}
+
+ shell_exec( 'cd ~/apps/wordpress/htdocs/ && git reset --hard HEAD && git pull' );
+
  
 ?>hi
 
