da53c98d8c8303e5c6f9ae4790ea4aaa74ea04c6
Scott Block <scottrblock@gmail.com>
2014-04-29 19:48:25 -0400

    sfoufjsdnf

--- github-post.php
+++ github-post.php
@@ -1,3 +1,10 @@
 <?php
-  shell_exec( 'git pull' );
-?>hi  
+ 
+// Use in the "Post-Receive URLs" section of your GitHub repo.
+ 
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd ~/apps/wordpress/htdocs/ && git reset --hard HEAD && git pull' );
+}
+ 
+?>hi
+
