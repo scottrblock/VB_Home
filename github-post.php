6824a990dfb12d7965503a48fe63e0c4505eafc9
Scott Block <scottrblock@gmail.com>
2014-04-29 19:55:51 -0400

    adsjasjld

--- github-post.php
+++ github-post.php
@@ -1,9 +1,1 @@
-<?php
- 
-// Use in the "Post-Receive URLs" section of your GitHub repo.
-
- shell_exec( 'cd ~/apps/wordpress/htdocs/ && git reset --hard HEAD && git pull' );
-
- 
-?>hi
-
+<?php `git pull`;
